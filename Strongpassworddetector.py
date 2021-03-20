cb9f4f2e83d3fe0a06f8332fde44302ebf646e79
Mohit Patni <mohitpatni0731@gmail.com>
2021-03-20 14:17:48 +0530

    Add files via upload

---
+++ Strongpassworddetector.py
@@ -0,0 +1,25 @@
+import re
+
+def uppercase_check(password):
+    if re.search('[A-Z]', password):
+        return True
+    return False
+
+def lowercase_check(password):
+    if re.search('[a-z]', password):
+        return True
+    return False
+
+def digit_check(password):
+    if re.search('[0-9]', password):
+        return True
+    return False
+
+def user_input_password_check():
+    password = input("Enter password : ")
+    if len(password) >= 8 and uppercase_check(password) and lowercase_check(password) and digit_check(password):
+        print("Strong Password")
+    else:
+        print("Weak Password")
+
+user_input_password_check()
